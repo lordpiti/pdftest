ce53db0192c59dee77163c31dc12181c39ce1f65
DESKTOP-MPG6RHJ\Pablo <lordpiti@gmail.com>
2024-12-09 00:22:30 +0000

    added dinktopdf and selectpdf

---
+++ pdftest.Core/PdfRenderers/DinkToPdfService.cs
@@ -0,0 +1,36 @@
+﻿using DinkToPdf;
+using System;
+
+namespace pdftest.Core.PdfRenderers
+{
+    public class DinkToPdfService
+    {
+        public void Generate(string htmlContent)
+        {
+            // Initialize the converter
+            var converter = new BasicConverter(new PdfTools());
+
+            // Set up the object that holds the options for PDF generation
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4,
+                    Out = @"sample-DinkToPdf.pdf"  // Output path for the generated PDF
+                },
+                Objects = {
+                    new ObjectSettings() {
+                        HtmlContent = htmlContent,
+                        WebSettings = { DefaultEncoding = "utf-8" },
+                        PagesCount = true, // Enable page numbers
+                    },
+                }
+            };
+
+            // Generate the PDF and save it to the specified output path
+            converter.Convert(doc);
+            Console.WriteLine("PDF has been generated successfully!");
+        }
+    }
+}
--- pdftest/Program.cs
+++ pdftest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using pdftest;
+using pdftest.Core.PdfRenderers;
 using pdftest.LiquidParsers;
 using pdftest.PdfRenderers;
 
@@ -6,37 +7,47 @@ var dotLiquidParser = new DotLIquidParser();
 var fluidParser = new FluidParser();
 var pdfServicePdfSharp = new PdfSharpService();
 var pdfServicePuppeteer = new PuppeteerService();
+var pdfServiceSelectPdf = new SelectPdfService();
+var pdfServiceDinkToPdf = new DinkToPdfService();
 
 System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
 string cssStr = File.ReadAllText(@"templates\styles.css");
-var htmlStr = File.ReadAllText(@"templates\test.html");
+var htmlStr = File.ReadAllText(@"templates\testwithcss.html");
+var htmlHeader = File.ReadAllText(@"templates\header.html");
 
-List<Product> productList = [
-    new Product {
-        Description = "the best product",
-        Name = "Product 1",
-        Price = 30 
-    },
-    new Product {
-        Description = "the worst product",
-        Name = "Product 2",
-        Price = 10
-    }];
+// Create a list to hold the products
+List<Product> productList = [];
+
+// Random number generator for prices
+var random = new Random();
+
+// Generate 50 products
+for (int i = 1; i <= 50; i++)
+{
+    productList.Add(new Product
+    {
+        Description = $"Product description {i}",
+        Name = $"Product {i}",
+        Price = random.Next(1, 101) // Random price between 1 and 100
+    });
+}
 var model = new { products = productList, pagetitle = "Test Page" };
 
 // DotLiquid example
 var htmlContent = dotLiquidParser.Parse(htmlStr, model);
 pdfServicePdfSharp.Generate(htmlContent, cssStr, "example-DotLiquid");
-await pdfServicePuppeteer.Generate(htmlContent, "templates/styles.css", "example-DotLiquid");
+//await pdfServicePuppeteer.Generate(htmlContent, "templates/styles.css", "example-DotLiquid");
+pdfServiceSelectPdf.Generate(htmlContent, htmlHeader);
+pdfServiceDinkToPdf.Generate(htmlContent);
 
-// Fluid example
-var parsedHtml = fluidParser.Parse(htmlStr, model);
-pdfServicePdfSharp.Generate(parsedHtml, cssStr, "example-Fluid");
-await pdfServicePuppeteer.Generate(parsedHtml, "templates/styles.css", "example-Fluid");
+//// Fluid example
+//var parsedHtml = fluidParser.Parse(htmlStr, model);
+//pdfServicePdfSharp.Generate(parsedHtml, cssStr, "example-Fluid");
+//await pdfServicePuppeteer.Generate(parsedHtml, "templates/styles.css", "example-Fluid");
 
-// Flexbox html example
-string cssStrFlex = File.ReadAllText(@"templates\stylesflex.css");
-var htmlStrFlex = File.ReadAllText(@"templates\testflexbox.html");
-pdfServicePdfSharp.Generate(htmlStrFlex, cssStrFlex, "example-flex");
-await pdfServicePuppeteer.Generate(htmlStrFlex, "templates/stylesflex.css", "example-flex");
+//// Flexbox html example
+//string cssStrFlex = File.ReadAllText(@"templates\stylesflex.css");
+//var htmlStrFlex = File.ReadAllText(@"templates\testflexbox.html");
+//pdfServicePdfSharp.Generate(htmlStrFlex, cssStrFlex, "example-flex");
+//await pdfServicePuppeteer.Generate(htmlStrFlex, "templates/stylesflex.css", "example-flex");
